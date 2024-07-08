8ad217c540f63452230ad0471da51b8840bce747
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2024-07-08 22:14:17 +0300

    final commit

--- expenses.py
+++ expenses.py
@@ -59,7 +59,6 @@ class Expenses:
         daily_budget = self.remaining_budget(budget) / remaining_days
         return daily_budget
 
-
 #create the main script
 def main():
     categories = ["🍔 Food", "🛒 Shopping", "🏠 Home", "🎉 Entertainment", "🚕 Transport"]
@@ -74,3 +73,51 @@ def main():
         print("5. View remaining budget")
         print("6. Exit")
         option = input("Enter your option: ")
+        if option == '1':
+            # display the valid categories to choose from
+            print("\n👉 Please select a category:")
+            for i, category in enumerate(categories):
+                print(f"{i+1}. {category}")
+            category = categories[int(input("Enter your category: ")) - 1]
+            description = input("Enter your description: ")
+            amount = float(input("Enter your amount: "))
+            # add the expense to the database
+            expenses.add_expenses(category, description, amount)
+            print("✔️  Your expense has been added successfully")
+
+        elif option == '2':
+            # display all expenses
+            expenses_list = expenses.view_all_expenses()
+            print("\n📝 All expenses:")
+            for expense in expenses_list:
+                print(f"{expense[1]} - {expense[2]} - {expense[3]} - {expense[4]}")
+
+        elif option == '3':
+            # display total expenses
+            total_expenses = expenses.total_expenses()
+            print(f"\n📊 Total expenses: {total_expenses:.2f}💲")
+
+        elif option == '4':
+            # display expenses of each category
+            category_totals = expenses.expenses_of_category()
+            print("\n📊 Expenses by category")
+            for category, total_amount in category_totals:
+                print(f"{category}:💲{total_amount:.2f}")
+
+        elif option == '5':
+            # display remaining budget
+            remaining_budget = expenses.remaining_budget()
+            print("\n💵 Budget")
+            print(f"You have💲{remaining_budget:.2f} left to spend this month")
+            print(f"That’s💲{expenses.daily_budget():.2f} per day")
+
+        elif option == '6':
+            # exit the program
+            print("\n👋 Thank you for using the expenses tracker! Have a great day!\n")
+            break
+
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
+    main()
