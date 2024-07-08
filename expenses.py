99e8bec9baeccad2ea490e0e666ca14606de13e7
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2024-07-08 20:07:50 +0300

    The base

--- expenses.py
+++ expenses.py
@@ -17,3 +17,27 @@ mycursor.execute("""
                  description VARCHAR(255), 
                  amount FLOAT, date DATE)
                  """)
+
+#The main class for tracking expenses
+class Expenses:
+    def __init__(self, description, amount, date):
+        self.description = description
+        self.amount = amount
+        self.date = date
+        self.categories = ["🍔 Food", "🛒 Shopping", "🏠 Home", "🎉 Entertainment, 🚕 Transport"]
+
+
+
+
+#create the main script
+def main():
+        while True:
+            print("📈 Welcome to the expenses tracker! \n")
+            print("👉 Please select an option:")
+            print("1. Add new expense")
+            print("2. View all expenses")
+            print("3. View total expenses")
+            print("4. View expenses by category")
+            print("5. View remaining budget")
+            print("6. Exit")
+            option = input("Enter your option: ")
